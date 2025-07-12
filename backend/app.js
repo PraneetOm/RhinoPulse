d7b50f989b5b49d02d9319cceb7291bbd1a23da9
Praneet Om <om.praneet.in@gmail.com>
2025-07-12 20:48:13 +0530

    Add files via upload

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import newsRoutes from './routes/news.js';
 
 dotenv.config();
 const app = express();
@@ -13,6 +14,7 @@ app.use(cors({
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/news', newsRoutes);
 
 // Routes
 import userRoutes from './routes/userRoutes.js';
