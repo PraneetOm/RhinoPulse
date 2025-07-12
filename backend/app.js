5b61a7381a4f6fd769c20b106324f43b23ed3981
Praneet Om <om.praneet.in@gmail.com>
2025-07-12 20:29:14 +0530

    Update app.js
    removed duplicate cors.

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import cors from 'cors';
 
 app.use(cors({
   origin: 'https://rhinopulse.netlify.app',
